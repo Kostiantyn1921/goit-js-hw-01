b3721d5866cc8b89630bcef17fde68aec9e260b3
Kostiantyn <retard15810@gmail.com>
2024-05-14 21:02:14 +0200

    delete gap

--- js/task-1.js
+++ js/task-1.js
@@ -1,5 +1,3 @@
-
-
 function makeTransaction(quantity, pricePerDroid) {
     return `You ordered ${quantity} droids worth ${quantity * pricePerDroid} credits!`;
 }
--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,3 @@
-
-
 function getShippingMessage(country, price, deliveryFee) {
     return `Shipping to ${country} will cost ${price + deliveryFee} credits`;
 }
